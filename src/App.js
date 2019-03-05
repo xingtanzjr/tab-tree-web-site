11ed4006d45ce88e39cc094e122db35c3a1cbfb8
Jinrui.Zhang <jinrzhan@microsoft.com>
2019-03-05 22:03:09 +0800

    change to logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import bjq from './images/bjq.jpg';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={bjq} width="500px" alt="Happy birthday"/>
+          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Happy birthday to Rail_bu!!!
           </p>
